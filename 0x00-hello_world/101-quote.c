86a8d967e15f7e2beac3a43fd46c5ab81e24c487
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-09-08 09:44:26 -0700

    Final 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - main function in the task
  *
- * Description: print a quote using write function
  *
- * return: return 1 (Success)
+ * Return: return 1
  */
-
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quo, 59);`
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
 
