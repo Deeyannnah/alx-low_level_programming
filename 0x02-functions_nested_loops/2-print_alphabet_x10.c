b81b9ebe8583fa03bc1339b73511f6bfcfa68d4b
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-09-13 16:10:22 +0100

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,18 @@
  * Return:void
  */
 
-void print_alphabet_x10(void)
+ void print_alphabet_x10(void)
 {
     int i = 0;
-    
-    while (i < 10)
+
+	while (i < 10)
     {
         char ch = 'a';
         while (ch <= 'z')
         {
             _putchar(ch);
             ch++;
+
         }
         _putchar('\n');
         i++;
