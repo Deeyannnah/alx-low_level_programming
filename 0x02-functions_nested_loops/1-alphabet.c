6136a49ad938665c4979f6b54aa7454e42cd4545
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-09-13 15:24:49 +0100

    1. I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(c);
+		ch++;
+	}
+	_putchar('\n');
+}
