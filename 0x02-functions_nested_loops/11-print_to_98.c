bd929a8f065c659861fe6ff6d5161d93f7dcca86
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-09-13 17:12:36 +0100

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,22 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_to_98 - print to 98
- * @n : number to begin from
- * Return:0 or 1
- */
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed by space
+ *           number should be printed in order
+ *
+ * @n: input
+*/
 
 void print_to_98(int n)
 {
+	int count;
 
-while (n < 98)
-{
-	printf("%i, ", n);
-	n++;
-}
-while (n > 98)
-{
-	printf("%i, ", n);
-	n--;
-}
-
-printf("98");
-putchar('\n');
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
