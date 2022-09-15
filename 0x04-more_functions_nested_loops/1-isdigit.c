63c4350042623a445d0bdaf91ec77680a9d0d0c7
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-09-15 14:12:58 +0100

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  * _isupper - Check if a letter is upper
+  * _isdigit - Check if a character is a digit
   * @c: The number to be checked
   *
-  * Return: 1 for upper letter or 0 for any else
+  * Return: 1 for a character that will be a digit or 0 for any else
   */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
