6a450111143fd42ae2417cbc140f57b4f86a97a9
Diana Etuk <dianabenjaminetuk@gmail.com>
2022-10-25 17:45:41 +0100

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,1 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * Desc: Header file for all prototypes in the 0x12-more_singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
 size_t print_listint(const listint_t *h);
